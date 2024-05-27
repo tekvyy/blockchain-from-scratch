d6c446e3cb3800b3743f6aeff131f1d695059dbd
tekvyy <divgun.gta2@gmail.com>
2024-05-27 12:24:22 +0800

    completed p1_switches.rs

--- src/c1_state_machine/p1_switches.rs
+++ src/c1_state_machine/p1_switches.rs
@@ -15,7 +15,7 @@ impl StateMachine for LightSwitch {
     type Transition = ();
 
     fn next_state(starting_state: &bool, t: &()) -> bool {
-        todo!("Exercise 1")
+        !starting_state
     }
 }
 
@@ -42,7 +42,29 @@ impl StateMachine for WeirdSwitchMachine {
     type Transition = Toggle;
 
     fn next_state(starting_state: &TwoSwitches, t: &Toggle) -> TwoSwitches {
-        todo!("Exercise 2")
+        let mut result: TwoSwitches = TwoSwitches { first_switch: false, second_switch: false };
+        match t {
+            Toggle::FirstSwitch => {
+                if starting_state.first_switch == true {
+                    result = TwoSwitches {
+                        first_switch: false,
+                        second_switch: false,
+                    };
+                }else {
+                    result = TwoSwitches {
+                        first_switch: true,
+                        second_switch: starting_state.second_switch,
+                    };
+                }
+            }
+            Toggle::SecondSwitch => {
+                result = TwoSwitches {
+                    first_switch: starting_state.first_switch,
+                    second_switch: !starting_state.second_switch,
+                };
+            }
+        }
+        result
     }
 }
 
